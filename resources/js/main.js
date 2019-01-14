582381b0e76d3b0a9c0c17d1131ac56bcd01c352
Jean Pierre <38929223+jpbsalibe@users.noreply.github.com>
2019-01-13 21:14:34 -0300

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.single-item').slick({ 
+      autoplay: true,
+      arrow: true,
+      prevArrow: "<button type='button' class='slick-prev pull-left'></button>", 
+      nextArrow: "<button type='button' class='slick-next pull-right'></button>", 
+  })
+  });
+
